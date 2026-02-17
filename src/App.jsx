5ad5eaebf806cfb0d0e3ae1ba37979dc5d9f9c89
Nitisart-123 <std.66122710110@ubru.ac.th>
2026-02-17 23:44:28 +0700

    เพิ่ม Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react';
 import MapComponent from './components/Map';
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <MapComponent/>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={styles.navbar}>
+      <div style={styles.logo}>Traffic System</div>
+
+      <div>
+        <button style={styles.button}>ตารางข้อมูล</button>
+        <button style={styles.button}>จัดการโหนดเซนเซอร์</button>
+      </div>
+    </nav>
+  );
+}
+
+const styles = {
+  navbar: {
+    backgroundColor: "#1976D2", // สีฟ้า
+    padding: "15px 30px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+  },
+  logo: {
+    fontSize: "20px",
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: "white",
+    color: "#1976D2",
+    border: "none",
+    padding: "8px 16px",
+    marginLeft: "10px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontWeight: "bold",
+  },
+};
+
+export default Navbar;
